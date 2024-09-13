ca778c1d8eed75b95a50b5902d94757b5057587b
Mayank Pal <mayankpal199523@gmail.com>
2024-09-13 23:24:29 +0530

    updated files from node-7-sept

---
+++ index.js
@@ -0,0 +1,83 @@
+const express=require('express');
+const users=require('./users.json');
+const app=express();
+app.use(express.json());
+const fs=require('fs');
+
+
+
+//http://localhost:3000/users/:10
+app.get('/users/:id',(req,res)=>{
+    let id=req.params.id;
+    let user=users.find((user)=>user.id ===parseInt(id))
+    console.log(user)
+    res.json(user);
+})
+
+// http://localhost:3000/add/user
+app.post('/add/user',(req,res)=>{
+    console.log(req.body);
+    req.body.id=31;
+    users.push(req.body);
+    fs.writeFile('users.json',JSON.stringify(users),(err)=>{
+        if(err){
+            console.log("error")
+        }
+        else{
+            console.log("data added successfully.......")
+            res.end("data added successfully")
+        }
+    })
+})
+// http://localhost:3000/users
+app.get('/users',(req,res)=>{
+    res.json(users);
+})
+
+// http://localhost:3000/edit/user/10
+app.put('/edit/user/:id',(req,res)=>{
+    let id=req.params.id;
+    console.log(id);
+    let index=users.findIndex((user)=>user.id===parseFloat(id));
+    users[index].first_name="rahul";
+    users[index].last_name="sharma";
+    res.end("upgrade in progess. ");
+    fs.writeFile('users.json',JSON.stringify(users),(err)=>{
+        if(err){
+            console.log("error")
+        }
+        else{
+            console.log("data updated successfully.......")
+            res.end("data updated successfully")
+        }
+    })
+})
+
+app.delete('/delete/user/:id',(req,res)=>{
+    let id=req.params.id;
+    console.log(id);
+    let index=users.findIndex((user)=>user.id===parseFloat(id));
+    users.splice(index,1);
+    res.end("deletion  in progess. ");
+    fs.writeFile('users.json',JSON.stringify(users),(err)=>{
+        if(err){
+            console.log("error")
+        }
+        else{
+            console.log("data updated successfully.......")
+            res.end("data updated successfully")
+        }
+    })
+})
+
+
+
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("server is running on 3000")
+    }
+})
